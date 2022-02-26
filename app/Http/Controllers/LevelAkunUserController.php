e30bfc301d155f0bf2912495e517daf316635c55
helenaputri <helenaputri38@gmail.com>
2022-02-26 19:05:49 +0700

    merge helena

---
+++ app/Http/Controllers/LevelAkunUserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\level_akun_user;
+use App\Http\Requests\Storelevel_akun_userRequest;
+use App\Http\Requests\Updatelevel_akun_userRequest;
+
+class LevelAkunUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\Storelevel_akun_userRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Storelevel_akun_userRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\level_akun_user  $level_akun_user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(level_akun_user $level_akun_user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\level_akun_user  $level_akun_user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(level_akun_user $level_akun_user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Updatelevel_akun_userRequest  $request
+     * @param  \App\Models\level_akun_user  $level_akun_user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Updatelevel_akun_userRequest $request, level_akun_user $level_akun_user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\level_akun_user  $level_akun_user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(level_akun_user $level_akun_user)
+    {
+        //
+    }
+}
